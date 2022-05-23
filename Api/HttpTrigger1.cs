511bd6521a26b1d8bfe3a0953ad58ac158535f91
Phil Cooper <phillycooper@hotmail.com>
2022-05-23 16:56:44 +0100

    Fixing build

--- Api/HttpTrigger1.cs
+++ Api/HttpTrigger1.cs
@@ -1,23 +1,35 @@
-using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Extensions.Options;
-using System;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Net.Http.Json;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
-namespace Api
+namespace Company.Function
 {
-    public class HttpTrigger1
+    public static class HttpTrigger1
     {
-        [FunctionName("Test")]
-        public IActionResult Logout([HttpTrigger(AuthorizationLevel.Function, "get", Route = "test")] HttpRequest request)
+        [FunctionName("HttpTrigger1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
         {
-            return new OkResult();
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
         }
     }
 }
